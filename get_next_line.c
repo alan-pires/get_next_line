21eb870221949cb96f0523757a87c7f28b5b4fa9
Alan <alan_pires2@hotmail.com>
2021-06-13 17:13:34 -0300

    treating read condition inside get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 17:25:08 by apires-d          #+#    #+#             */
-/*   Updated: 2021/06/13 16:33:09 by apires-d         ###   ########.fr       */
+/*   Updated: 2021/06/13 17:11:48 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,45 @@
 # ifndef BUFFER_SIZE
 # define BUFFER_SIZE 9
 # endif
+
+static void	ft_free(char **p)
+{
+	if (*p)
+	{
+		free(*p);
+		*p = NULL;
+	}
+}
+
+static char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*p;
+	size_t	len;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	p = (char *)malloc((len + 1) * sizeof(*p));
+	if (p == NULL)
+		return (NULL);
+	while (*s1)
+	{
+		p[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2)
+	{
+		p[i] = *s2;
+		i++;
+		s2++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
 
 char	*ft_calloc(size_t count, size_t size)
 {
@@ -57,7 +96,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	return (dest);
 }
 
- size_t	ft_strlen(const char *s)
+static size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
@@ -67,7 +106,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	return (i);
 }
 
-char	*ft_strdup(const char *s1)
+static char	*ft_strdup(const char *s1)
 {
 	char	*copy;
 	size_t	i;
@@ -87,111 +126,7 @@ char	*ft_strdup(const char *s1)
 	return (copy);
 }
 
-int	ft_save_str(char *str, size_t size)
-{
-	char	*aux;
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = (ft_strlen(str) - size) + 1;
-	aux = malloc(len);
-	if (!aux)
-		return (0);
-	while (i < len)
-		aux[i++] = str[size++];
-	aux[i] = '\0';
-	free(str);
-	str = malloc(len);
-	str = ft_strdup(aux);
-	return (1);
-}
-
-int	ft_get_line(char *str, char **line)
-{
-	size_t	i;
-	size_t	j;
-	char	*aux;
-
-	i = 0;
-	j = 0;
-	while (str[i] && str[i] != '\n' && i < BUFFER_SIZE)
-		i++;
-	if (str[i] == '\n')
-	{
-		if (ft_strlen(str) <= BUFFER_SIZE)
-		{
-			*line = str;
-			//printf("%s\n", aux);
-		}
-		else
-		{
-			aux = malloc(i + 1);
-			if (!aux)
-				return (0);
-			while (j < i)
-				aux[j++] = str[i++];
-			aux[j] = '\0';
-			*line = aux;
-			//printf("%s\n", aux);
-			ft_save_str(str, i);
-		}
-	}
-	else
-	{
-		if (ft_strlen(str) <= BUFFER_SIZE)
-		{
-			*line = str;
-			//printf("%s\n", aux);
-		}
-		else
-		{
-			aux = malloc(i + 1);
-			if (!aux)
-				return (0);
-			while (j < i)
-				aux[j++] = str[i++];
-			aux[j] = '\0';
-			*line = aux;
-			//printf("%s\n",*line);
-			ft_save_str(str, i);
-		}
-	}
-	return (1);
-}
-
-int	ft_mount_line(char *str, char *buff)
-{
-	char	*aux;
-	size_t	len;
-
-	if (str != NULL && str != "")
-	{
-		len = ft_strlen(str) + ft_strlen(buff) + 1;
-		aux = malloc(len);
-		if (!aux)
-			return (0);
-		//printf("%s\n", buff);
-		aux = ft_strncat(str, buff, len);
-		
-		free(str);
-		str = malloc(len);
-		if (!str)
-			return (0);
-		str = ft_strdup(aux);
-		free(aux);
-	}
-	else
-	{
-		len = ft_strlen(buff + 1);
-		str = malloc(len);
-		if (!str)
-			return (0);
-		str = ft_strdup(buff);
-	}
-	return (1);
-}
-
+/*
 ssize_t	ft_read_line(int fd, char *str, char *buff, char **line)
 {
 	ssize_t		read_b;
@@ -206,6 +141,7 @@ ssize_t	ft_read_line(int fd, char *str, char *buff, char **line)
 		return (-1);
 	return (read_b);
 }
+*/
 
 int	get_next_line(int fd, char **line)
 {
@@ -227,7 +163,10 @@ int	get_next_line(int fd, char **line)
 		return (-1);
 	while (read(fd, buff, BUFFER_SIZE) > 0)
 	{
-
+		r_bytes = ft_strlen(buff);
+		buff[r_bytes] = '\0';
+		aux = ft_strjoin(content[fd], buff);
+		ft_free(&content[fd]);
 	}
 	return ();
 }
