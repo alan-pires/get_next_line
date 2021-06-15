6a256d0ac49582cee975a2f4f514c458d40800f8
Alan <alan_pires2@hotmail.com>
2021-06-15 14:58:16 -0300

    testing one fd per time

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,22 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 17:25:08 by apires-d          #+#    #+#             */
-/*   Updated: 2021/06/15 12:40:32 by apires-d         ###   ########.fr       */
+/*   Updated: 2021/06/15 14:40:21 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static void	ft_free(char **p)
+static void	ft_free(char *p)
 {
-	if (*p && p)
+	if (*p)
 	{
-		free(*p);
-		*p = NULL;
+		free(p);
+		p = NULL;
 	}
 }
 
-static int	ft_get_line(char **line, int r_bytes, char **content)
+static int	ft_get_line(char **line, int r_bytes, char *content)
 {
 	int		i;
 	char	*aux;
@@ -29,32 +29,32 @@ static int	ft_get_line(char **line, int r_bytes, char **content)
 	i = 0;
 	if (r_bytes < 0)
 		return (-1);
-	else if (*content[0] == '\0' && r_bytes == 0)
+	else if (content[0] == '\0' && r_bytes == 0)
 	{
 		*line = ft_strdup("");
 		ft_free(content);
 		return (0);
 	}
-	while ((*content)[i] != '\0' && (*content)[i] != '\n')
+	while (content[i] != '\0' && content[i] != '\n')
 		i++;
-	*line = ft_substr(*content, 0, i);
-	if ((*content)[i] == '\n')
+	*line = ft_substr(content, 0, i);
+	if (content[i] == '\n')
 	{
-		aux = ft_strdup(*content + i + 1);
-		free(*content);
-		*content = aux;
+		aux = ft_strdup(content + i + 1);
+		free(content);
+		content = aux;
 		return (1);
 	}
 	ft_free(content);
 	return (0);
 }
 
-static int	ft_init_content(char **content)
+static int	ft_init_content(char *content)
 {
-	if (!(*content))
+	if (!(content))
 	{
-		*content = ft_strdup("");
-		if (!(*content))
+		content = ft_strdup("");
+		if (!(content))
 			return (-1);
 	}
 	return (0);
@@ -70,13 +70,13 @@ int	get_next_line(int fd, char **line)
 {
 	char		*buff;
 	char		*aux;
-	static char	*content[RLIMIT_NOFILE];
+	static char	*content;
 	ssize_t		r_bytes;
 
 	r_bytes = 0;
 	if (fd < 0 || !line || BUFFER_SIZE < 1)
 		return (-1);
-	if (ft_init_content(&(content[fd])) != 0)
+	if (ft_init_content(content) != 0)
 		return (-1);
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
@@ -84,12 +84,12 @@ int	get_next_line(int fd, char **line)
 	while (ft_read(&fd, buff, &r_bytes) > 0)
 	{
 		buff[r_bytes] = '\0';
-		aux = ft_strjoin(content[fd], buff);
-		ft_free(&content[fd]);
-		content[fd] = aux;
+		aux = ft_strjoin(content, buff);
+		ft_free(content);
+		content = aux;
 		if (ft_strchr(buff, '\n'))
 			break ;
 	}
 	free(buff);
-	return (ft_get_line(line, r_bytes, &(content[fd])));
+	return (ft_get_line(line, r_bytes, content));
 }
